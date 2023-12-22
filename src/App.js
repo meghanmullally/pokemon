26a3ff1f59d03649d2377585acda1dacf595b230
Meghan Mullally <ms.mullally27@gmail.com>
2023-12-22 12:18:24 -0800

    created id for pokemon in fetch

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import { useEffect, useState } from 'react';
 import Pokedex from './components/Pokedex/Pokedex';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Pokemon from './components/Pokemon';
-import ErrorPage from './components/Error';
+// import ErrorPage from './components/Error';
 import RootLayout from './Root';
 
 function App() {
 
-  let pokemonList = [];
-
   // give variable name, then set it with function..place in memory to useState to keep track of it...
   // example in Max's react course - destructoring 
   const [pokemonData, setPokemonData] = useState([]);
+  // help to determine if data is still being fetched or if it has loaded successfully 
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // the Pokémon API (PokeAPI) contains information on the first 898 Pokémon species, which includes all Pokémon from Generations I to VIII.
@@ -26,7 +26,17 @@ function App() {
       })
       .then((data) => {
         console.log("data.results = ", data.results);
-        setPokemonData(data.results);
+        const modifiedPokemonData = data.results.map((pokemon, index) => ({
+          ...pokemon,
+          id: index + 1, // Add 1 to index to create a unique ID for each Pokemon
+        }));
+        console.log("modifiedPokemonData =", modifiedPokemonData);
+        setPokemonData(modifiedPokemonData);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching Pokemon Data', error);
+        setLoading(false);
       });
 
   }, []);
@@ -34,8 +44,8 @@ function App() {
   console.log("pokemonData ", pokemonData);
 
   const router = createBrowserRouter([
-        { path: '/', element: <Pokedex pokemonData={pokemonData}/> },
-        { path: '/pokemon/:pokemonId', element: <Pokemon /> },
+    { path: '/', element: !loading ? <Pokedex pokemonData={pokemonData} /> : null },
+    { path: '/pokemon/:pokemonId', element: <Pokemon /> },
   ]);
 
   return (
