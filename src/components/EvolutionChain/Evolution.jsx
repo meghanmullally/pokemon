9d30012d9c667eeb217c0e0998d25c3176ad07ba
Meghan Mullally <ms.mullally27@gmail.com>
2024-01-16 23:06:06 -0800

    move fetch call to Pokemon.jsx file, change single quotes to double quotes for styling consistancy

--- src/components/EvolutionChain/Evolution.jsx
+++ src/components/EvolutionChain/Evolution.jsx
@@ -1,71 +1,47 @@
-import React, { useEffect, useState } from "react";
-import { Paper, Typography } from '@mui/material';
-import PokemonCard from '../PokemonCard/PokemonCard';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import { POKEMON_LIMIT } from '../../constants/pokemon';
-import './Evolution.css';
+import React from "react";
+import { Paper, Typography } from "@mui/material";
+import PokemonCard from "../PokemonCard/PokemonCard";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import { POKEMON_LIMIT } from "../../constants/pokemon";
+import "./Evolution.css";
 
 
-export default function Evolution({ pokemonSpecies }) {
+export default function Evolution({ evolutionData }) {
+  //   const { evolutionData } = props;
 
-    const { id } = pokemonSpecies
-    const [evolutionDetails, setEvolutionDetails] = useState(null);
+  console.log("--------------------------------");
+  console.log("IN EVOLUTION COMP");
+  console.log("evolutionData", evolutionData);
 
-    console.log('--------------------------------');
-    console.log('IN EVOLUTION COMP');
-    console.log('pokemonSpecies', pokemonSpecies);
+  return (
+    <>
+      <Paper>
+        <Typography variant="h3">Evolution Chain</Typography>
+        <div className="evoContainer">
+          <div className="evoPoke">
+            {evolutionData.map((pokemon) => {
+              const { id, name, sprite } = pokemon;
+              console.log("id", id);
+              console.log("pokemon", pokemon);
 
-    useEffect(() => {
-
-        const evolutionChainUrl = `https://pokeapi.co/api/v2/evolution-chain/${id}/`;
-        fetch(evolutionChainUrl)
-            .then((response) => response.json())
-            .then((data) => {
-                console.log("IN EVO - evo data", data);
-                setEvolutionDetails(data);
-            })
-            .catch((error) => {
-                console.error('Error fetching Evo Data:', error);
-            });
-
-    }, [])
-    // end of useEffect
-
-    console.log("EvoDetails", evolutionDetails);
-
-    // checking for evolutionDetails
-    if (!evolutionDetails) {
-        console.log('No evolutionDetails found');
-        return null; // or some loading indicator
-    }
-
-
-    return (
-
-        <>
-            <Paper>
-                <Typography variant="h3">Evolution Chain</Typography>
-                <div className="evoContainer">
-                    <div className="evoPoke">
-
-                        <div className='evoArrow'>
-                            <KeyboardArrowRightIcon />
-                        </div>
-                        <div className='evoLevel'>
-                            {/* min level of evolution detail */}
-                            Level {evolutionDetails.chain.evolves_to[0].evolution_details[0].min_level}
-                        </div>
-
+              if (parseInt(id) > POKEMON_LIMIT) {
+                return null;
+              } else {
+                return (
+                  <React.Fragment key={pokemon.id}>
+                    <div className="evoCard">
+                      <PokemonCard pokemonId={pokemon} />
                     </div>
-                </div>
-            </Paper>
-
-        </>
-
-
-
-    )
-
-
-
+                    <div className="evoArrow">
+                      <KeyboardArrowRightIcon />
+                    </div>
+                  </React.Fragment>
+                );
+              }
+            })}
+          </div>
+        </div>
+      </Paper>
+    </>
+  );
 }
