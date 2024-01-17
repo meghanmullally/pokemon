6bc18d940e6c1fa2b754d353a6fe625659a54683
Meghan Mullally <ms.mullally27@gmail.com>
2024-01-16 23:04:56 -0800

    make page more consistent double quotes vs single quotes, remove pokemonId and leave name for card

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,56 +1,55 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Divider, Paper } from '@mui/material';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Divider, Paper } from "@mui/material";
 // import { TYPE_COLORS } from '../../constants/pokemon';
-import './PokemonCard.css';
+import "./PokemonCard.css";
 
 export default function PokemonCard(props) {
+  const { pokemonId } = props;
+  // Destructuring properties from pokemonId
+  const { id, name, sprite } = pokemonId;
 
-    const { pokemonId } = props;
-    // Destructuring properties from pokemonId
-    const { id, name, sprite} = pokemonId;
+  // Type Colors and Card Background Color
+  // const getBackgroundColor = (types) => {
+  //     if (types && types.length > 0) {
+  //         const firstType = types[0].type.name;
+  //         const color = TYPE_COLORS[firstType] || 'white'; // Default to white if color is not found
+  //         return `${color}30`; // Add 80 (hexadecimal for 128, which is 50% opacity) to the color
+  //     } else {
+  //         // Handle the case when types is undefined or an empty array
+  //         return 'white30'; // You can provide a default color or handle it as needed
+  //     }
+  // };
 
-    // Type Colors and Card Background Color
-    // const getBackgroundColor = (types) => {
-    //     if (types && types.length > 0) {
-    //         const firstType = types[0].type.name;
-    //         const color = TYPE_COLORS[firstType] || 'white'; // Default to white if color is not found
-    //         return `${color}30`; // Add 80 (hexadecimal for 128, which is 50% opacity) to the color
-    //     } else {
-    //         // Handle the case when types is undefined or an empty array
-    //         return 'white30'; // You can provide a default color or handle it as needed
-    //     }
-    // };
-
-    return (
-        <>
-            <NavLink className="nav-link" to={{ pathname: `/pokemon/${id}` }}>
-                <Paper className='pokemonCardPaper'>
-                    <Card
-                        // style=
-                        // {
-                        //     { backgroundColor: pokemonDetails ? getBackgroundColor(pokemonDetails.types) : 'white30' }
-                        // }
-                        >
-                        <CardMedia
-                            component="img"
-                            alt="pokemon image"
-                            height={{ height: '100%' }}
-                            image={sprite} // default image
-                        />
-                        <Divider variant="middle" />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {pokemonId && name}
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Paper>
-            </NavLink>
-        </>
-    );
+  return (
+    <>
+      <NavLink className="nav-link" to={{ pathname: `/pokemon/${id}` }}>
+        <Paper className="pokemonCardPaper">
+          <Card
+          // style=
+          // {
+          //     { backgroundColor: pokemonDetails ? getBackgroundColor(pokemonDetails.types) : 'white30' }
+          // }
+          >
+            <CardMedia
+              component="img"
+              alt="pokemon image"
+              height={{ height: "100%" }}
+              image={sprite} // default image
+            />
+            <Divider variant="middle" />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {name}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Paper>
+      </NavLink>
+    </>
+  );
 }
