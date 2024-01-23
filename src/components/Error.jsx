1a75237d18524c79e55247a097a2f79cda81d0ce
Meghan Mullally <ms.mullally27@gmail.com>
2024-01-22 21:27:10 -0800

    update Error component to use Header vs Navigation comp

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,10 +1,10 @@
-import { Navigation } from "@mui/icons-material";
+import Header from "./Header/Header";
 
 function ErrorPage() {
 
     return (
         <>
-            <Navigation />
+            <Header />
             <main>
                 <h1>An error occured!</h1>
                 <p>Could not find this page!</p>
