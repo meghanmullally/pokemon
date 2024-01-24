7d106b56cad1231f7282860a2065170f788c69bf
Meghan Mullally <ms.mullally27@gmail.com>
2024-01-23 21:45:56 -0800

    move Header from Apps into Pokedex, change pokemonItem to pokemonId to align with the other files

--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -1,26 +1,37 @@
 import React from "react";
 import PokemonCard from "../PokemonCard/PokemonCard";
-import { Box, Grid } from "@mui/material";
+import Header from '../Header/Header';
+import { Box, Grid, CircularProgress } from "@mui/material";
 import "./Pokedex.css";
 
-const Pokedex = ({ pokemonData }) => {
+const Pokedex = ({ pokemonData, optionData }) => {
+
   return (
+<>
+  <Header optionData={optionData}/>
     <Box sx={{ width: "100%" }}>
+    { pokemonData !== null ? (
       <Grid
         container
         rowSpacing={2}
         columnSpacing={{ xs: 1, sm: 2, md: 3 }}
         className="pokedexContainer"
-      >
+        >
         {/* Mapping through the pokemon data that is sent over from App.js and displaying the pokemon's name */}
         {pokemonData &&
-          pokemonData.map((pokemonItem) => (
-            <Grid item xs={2} key={pokemonItem.name}>
-              <PokemonCard pokemonId={pokemonItem} />
+          pokemonData.map((pokemonId) => (
+            <React.Fragment key={pokemonId.id}>
+            <Grid item xs={2}>
+              <PokemonCard pokemonId={pokemonId} />
             </Grid>
+            </React.Fragment>
           ))}
       </Grid>
+      ) : (
+        <CircularProgress/>
+      )}
     </Box>
+</>
   );
 };
 
