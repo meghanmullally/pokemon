203f2cd28da4f72b65411f9374a0e9404cf69efa
Meghan Mullally <ms.mullally27@gmail.com>
2024-01-31 22:02:47 -0800

    add filterSearch to Pokedex and add search data into header, filter when searching

--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -1,36 +1,39 @@
 import React from "react";
 import PokemonCard from "../PokemonCard/PokemonCard";
 import Header from '../Header/Header';
-import { Box, Grid, CircularProgress } from "@mui/material";
+import { Grid, CircularProgress } from "@mui/material";
+import { useAppSelector } from '../../app/hooks';
 import "./Pokedex.css";
 
-const Pokedex = ({ pokemonData, optionData }) => {
+const Pokedex = ({ searchOptionData })  => {
+
+  const pokemonData = useAppSelector(state => state.pokemon.pokemonData);
+  const filterSearch = useAppSelector(state => state.pokemon.filterSearch);
 
   return (
-<>
-  <Header optionData={optionData}/>
-    <Box sx={{ width: "100%" }}>
+    <>
+  <Header searchOptionData={searchOptionData}/>
     { pokemonData !== null ? (
       <Grid
-        container
-        rowSpacing={2}
-        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-        className="pokedexContainer"
-        >
-        {/* Mapping through the pokemon data that is sent over from App.js and displaying the pokemon's name */}
+      container
+      rowSpacing={2}
+      columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+      className="pokedexContainer"
+      >
         {pokemonData &&
-          pokemonData.map((pokemonId) => (
-            <React.Fragment key={pokemonId.id}>
-            <Grid item xs={2}>
-              <PokemonCard pokemonId={pokemonId} />
-            </Grid>
-            </React.Fragment>
+          Object.keys(pokemonData).map((pokemonId) => 
+          (
+            pokemonData[pokemonId].name.includes(filterSearch) &&
+              <React.Fragment key={pokemonId}>
+              <Grid item xs={2}>
+                <PokemonCard pokemonId={pokemonId} />
+              </Grid>
+              </React.Fragment>
           ))}
       </Grid>
       ) : (
         <CircularProgress/>
-      )}
-    </Box>
+        )}
 </>
   );
 };
