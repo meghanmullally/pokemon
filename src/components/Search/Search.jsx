d593657612c117f728105726a5c3b0ed05869e77
Meghan Mullally <ms.mullally27@gmail.com>
2024-01-31 22:01:55 -0800

    add redux to Search.jsx file and create a groupby find that creates a lastest search in header

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,33 +2,46 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Autocomplete from '@mui/material/Autocomplete';
+import { useAppSelector } from '../../app/hooks';
 import './Search.css';
 
-export default function Search({ searchOptionData }) {
-  
-  const options = searchOptionData ? searchOptionData.map((option) => option.name) : [];
+export default function Search({ onChange, label }) {
+
+  const searchOptionData = useAppSelector(state => state.pokemon.searchOptionData);
+  const historyData = useAppSelector(state => state.pokemon.historyData);
   const latestSearches = [];
 
-  console.log("searchOptionData", searchOptionData);
 
   return (
     <Stack spacing={2} sx={{ width: 300 }} className='autoCompleteContainer'>
       <Autocomplete
         freeSolo
-        id="free-solo-2-demo"
+        id="searchPokemon"
         disableClearable
         autoSelect
-        options={options}
-       // add a group
-        groupBy={(option) => option}
+        options={searchOptionData ? searchOptionData.map((option) => option.name) : []}
+        groupBy={(name) => {
+          let header = '';
+          if (historyData.find(element => element.name === name)) {
+            if (!latestSearches.includes(name)) {
+              latestSearches.push(name);
+            }
+            header = 'Latest Searches';
+          } else {
+            header = name[0].toUpperCase();
+          }
+          return header;
+        }
+        }
         renderInput={(params) => (
           <TextField
             {...params}
-            label="Search for Pokemon"
+            label={label}
             InputProps={{
               ...params.InputProps,
               type: 'search',
             }}
+            onChange={onChange}
           />
         )}
       />
