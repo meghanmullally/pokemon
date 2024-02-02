e45dd33c6793b67d90678f01a8d309f74a5da604
Meghan Mullally <ms.mullally27@gmail.com>
2024-02-02 00:00:40 -0800

    add moves definition to move tooltip

--- src/components/Moves/Moves.jsx
+++ src/components/Moves/Moves.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  Paper, Button, Tooltip } from '@mui/material';
+import { Paper, Button, Tooltip } from '@mui/material';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import './Moves.css';
 
@@ -12,7 +12,8 @@ const Moves = ({ pokemonDetails }) => {
         <h3 className="movesTitle">
           Moves:
           <Tooltip
-            title="moves..."
+          // https://pokemon.fandom.com/wiki/Move Source for Move definition 
+            title="A Move is a ability that a Pokémon uses during Pokémon Battles. Moves are mainly used to inflict damage on the opponent. Moves usually come from a natural ability that the specific Pokémon has."
             arrow
           >
             <InfoOutlinedIcon className="infoIcon" />
