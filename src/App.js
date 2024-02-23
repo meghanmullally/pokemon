0799b69d8bbc2f470121f625adb100e0cbc113c2
Meghan Mullally <ms.mullally27@gmail.com>
2024-02-22 18:16:12 -0800

    go back to using redux instead of useMemo..will need to do more research

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { pokemonActions } from './components/PokemonSlice';
 import { useAppDispatch } from "./app/hooks";
@@ -7,41 +7,27 @@ import Pokemon from "./components/Pokemon/Pokemon";
 import { POKEMON_LIMIT } from "./constants/pokemon";
 import "./App.css";
 
-
 function App() {
-  
-  const pokemonData ={};
+
+  const pokemonData = {};
   const searchOptionData = [];
   const dispatch = useAppDispatch();
-
   // help to determine if data is still being fetched or if it has loaded successfully
   const [loading, setLoading] = useState(true);
-
   // Pokemon Image
-  // useMemo ensures that the function is only recreated when the inputID dependency changes.
-  const generatedPokemonImageUrl = useMemo(() => (inputID) => {
+  const generatedPokemonImageUrl = (inputID) => {
     const defaultPokemonImageUrl = `https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${inputID}.svg`;
     return inputID
       ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${inputID}.png`
       : defaultPokemonImageUrl;
-  }, []);
-  
+  };
 
   useEffect(() => {
-    // the Pokémon API (PokeAPI) contains information on the first 898 Pokémon species, which includes all Pokémon from Generations I to VIII.
     const url = `https://pokeapi.co/api/v2/pokemon?limit=${POKEMON_LIMIT}`;
-
-  // Use useMemo to memoize the initialization of memoizedPokemonData
-  const memoizedPokemonData = useMemo(() => ({}), []);
-
-  // Use useMemo to memoize the initialization of memoizedSearchOptionData
-  const memoizedSearchOptionData = useMemo(() => [], []);
-
 
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-
         data.results.forEach((pokemon, index) => {
           let pokemonImgId = (index + 1).toString();
           const sprite = generatedPokemonImageUrl(pokemonImgId);
@@ -59,20 +45,20 @@ function App() {
           const nameA = a.name.toUpperCase();
           const nameB = b.name.toUpperCase();
           return nameA <= nameB ? -1 : 1;
-      });
+        });
 
-        dispatch(pokemonActions.setPokemonData(memoizedPokemonData));
-        dispatch(pokemonActions.setSearchOptionData(memoizedSearchOptionData));
+        dispatch(pokemonActions.setPokemonData(pokemonData));
+        dispatch(pokemonActions.setSearchOptionData(searchOptionData));
         setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching Pokemon Data", error);
         setLoading(false);
       });
-  }, [dispatch]);
+  }, [dispatch, generatedPokemonImageUrl, pokemonData, searchOptionData]);
 
   const router = createBrowserRouter([
-    { path: "/", element: !loading && <Pokedex pokemonData={pokemonData} searchOptionData={searchOptionData}/> },
+    { path: "/", element: !loading && <Pokedex pokemonData={pokemonData} searchOptionData={searchOptionData} /> },
     { path: "/pokemon/:pokemonId", element: <Pokemon /> },
   ]);
 
@@ -80,9 +66,8 @@ function App() {
     <div className="App">
       <RouterProvider router={router} />
       <br />
-      {/* <a href="https://icons8.com/icon/63311/pokeball">Pokeball</a> icon by <a href="https://icons8.com">Icons8</a> */}
     </div>
   );
-};
+}
 
 export default App;
