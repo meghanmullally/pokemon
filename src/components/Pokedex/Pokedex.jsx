bb0ea07cc78ee7cf153e8673a2634e32d19aa759
Meghan Mullally <ms.mullally27@gmail.com>
2024-07-17 13:29:51 -0700

    fix the pokedex grid to be responsive and updated the padding to be shorehand

--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -5,36 +5,36 @@ import { Grid, CircularProgress } from "@mui/material";
 import { useAppSelector } from '../../app/hooks';
 import "./Pokedex.css";
 
-const Pokedex = ({ searchOptionData })  => {
+const Pokedex = ({ searchOptionData }) => {
 
   const pokemonData = useAppSelector(state => state.pokemon.pokemonData);
   const filterSearch = useAppSelector(state => state.pokemon.filterSearch);
 
   return (
     <>
-  <Header searchOptionData={searchOptionData}/>
-    { pokemonData !== null ? (
-      <Grid
-      container
-      rowSpacing={2}
-      columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-      className="pokedexContainer"
-      >
-        {pokemonData &&
-          Object.keys(pokemonData).map((pokemonId) => 
-          (
-            pokemonData[pokemonId].name.includes(filterSearch) &&
+      <Header searchOptionData={searchOptionData} />
+      {pokemonData !== null ? (
+        <Grid
+          container
+          rowSpacing={2}
+          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          className="pokedexContainer"
+        >
+          {pokemonData &&
+            Object.keys(pokemonData).map((pokemonId) =>
+            (
+              pokemonData[pokemonId].name.includes(filterSearch) &&
               <React.Fragment key={pokemonId}>
-              <Grid item xs={2}>
-                <PokemonCard pokemonId={pokemonId} />
-              </Grid>
+                <Grid item xs={12} sm={6} md={3} lg={3} xl={2} >
+                  <PokemonCard pokemonId={pokemonId} />
+                </Grid>
               </React.Fragment>
-          ))}
-      </Grid>
+            ))}
+        </Grid>
       ) : (
-        <CircularProgress/>
-        )}
-</>
+        <CircularProgress />
+      )}
+    </>
   );
 };
 
