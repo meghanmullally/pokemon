ddfbec571281fd67013f70119a1f34fa1279be99
meghanmullally <ms.mullally27@gmail.com>
2024-09-27 14:59:20 -0700

    center and make progress circle bigger

--- src/components/LoadingMessage/LoadingMessage.jsx
+++ src/components/LoadingMessage/LoadingMessage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { CircularProgress } from '@mui/material';
-
+import './LoadingMessage.css';
 
 const LoadingMessage = () => {
   return (
-    <>
-      <CircularProgress color="primary" variant="indeterminate" />
-    </>
+    <div className="loadingContainer">
+      <CircularProgress color="primary" variant="indeterminate" size={150} thickness={3} className="loadingCircle"/>
+    </div>
   );
 };
 
